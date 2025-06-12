355adbd8ab304a2848b244737400aa82fb107d99
.git/xamirdeveloper <developer.salari@gmail.com>
2025-06-12 21:37:28 +0330

    config pwa 2

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-card class="ap-bg-primary-200" width="300" height="300"></v-card>
+  <v-card class="ap-bg-primary-200 ap-txt-title-1" width="300" height="300">test server</v-card>
 </template>
 
 <script lang="ts" setup></script>
